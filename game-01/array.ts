ad795123cdb50ef276b0f19038014b7b95cc6345
Carlos Amoros <camorost@gmail.com>
2021-02-26 00:48:25 -0500

    game-01: solved return typing issue

--- game-01/array.ts
+++ game-01/array.ts
@@ -1,17 +1,16 @@
 export class NumbersArray {
   getFirstPair(numbersArray: number[], pairSum: number): [number, number] {
-    const hash = [];
-    const matchingPair = [];
+    const hash: (number | undefined)[] = [];
+    let matchingPair: [number, number];
 
     for (let i = 0; i < numbersArray.length; i++) {
       if (hash[numbersArray[i]] != null) {
-        matchingPair[0] = hash[numbersArray[i]];
-        matchingPair[1] = numbersArray[i];
+        matchingPair = [hash[numbersArray[i]], numbersArray[i]];
         break;
       } else {
         hash[pairSum - numbersArray[i]] = numbersArray[i];
       }
     }
-    return [matchingPair[0], matchingPair[1]];
+    return matchingPair;
   }
 }
