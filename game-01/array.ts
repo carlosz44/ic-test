583398e811bf06c88bf513e0151c115e7a772f9b
Carlos Amoros <camorost@gmail.com>
2021-02-25 22:18:46 -0500

    updated game-01 solution

--- game-01/array.ts
+++ game-01/array.ts
@@ -1,20 +1,18 @@
-class NumbersArray {
+export class NumbersArray {
   numbersArray: number[];
   pairSum: number;
 
   getFirstPair(numbersArray: number[], pairSum: number): [number, number] {
-    // Hashing
     const hash = [];
     const matchingPair = [];
 
     for (let i = 0; i < numbersArray.length; i++) {
       if (hash[numbersArray[i]] != null) {
-        const index = hash[numbersArray[i]];
-        matchingPair[0] = index;
-        matchingPair[1] = i;
+        matchingPair[0] = hash[numbersArray[i]];
+        matchingPair[1] = numbersArray[i];
         break;
       } else {
-        hash[pairSum - numbersArray[i]] = i;
+        hash[pairSum - numbersArray[i]] = numbersArray[i];
       }
     }
     return [matchingPair[0], matchingPair[1]];
